955f174ba08ca8f85a4bec9e9dd0ce05d72b5979
Mawit <mawitgaddev@gmail.com>
2026-02-12 19:18:23 +0100

    add the google tag for analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { Toaster } from 'react-hot-toast';
+import Script from "next/script";
 
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
@@ -63,6 +64,21 @@ export default function RootLayout({
     return (
         <html lang="en" suppressHydrationWarning>
             <body className={`${inter.variable} antialiased`}>
+
+                {/* Google Analytics */}
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-21YYHLM69N"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-21YYHLM69N');
+          `}
+                </Script>
+
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="dark"
